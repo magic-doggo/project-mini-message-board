8382c1d9763b8e220e3cf2a4c9bda4830355146a
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-06-10 05:56:42 +0000

    Add index route and ejs template to render messages

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const path = require("node:path");
+
+const indexRouter = require("./routes/indexRouter");
+app.use("/", indexRouter);
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
